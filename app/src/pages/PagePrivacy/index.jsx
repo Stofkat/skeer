dbec3286e9cb4caf7ee970ec4f16dd1aa48454fa
Dirk de Boer <stofkat@gmail.com>
2023-03-23 16:43:12 +0100

    updated privacy policy

--- app/src/pages/PagePrivacy/index.jsx
+++ app/src/pages/PagePrivacy/index.jsx
@@ -4,16 +4,41 @@ import "./style.scss";
 const PagePrivacy = () => {
   return (
     <div className="page-privacy">
-      <h1>Privacy statement</h1>
-      Alle gegevens die je invoert op Skeer staan alleen lokaal opgeslagen binnen je brower (of app).
-      Door simpelweg je browser geschiedenis te verwijderen of de app te deinstalleren zijn al deze gegevens weg.
-      <br /><br />
-      We werken aan de mogelijkheid om deze gegevens aan een persoonlijk account te koppelen, zodat je push notificaties
-      kan instellen voor bepaalde producten.
-      <br /><br />
-      Dit is volledig optioneel. We vinden het zelf ook niet prettig als onze gegevens ongevraagd worden opgeslagen en doen dus ook zelf niet.
-      <br/><br/>
-      Ok doei.
+      <div className="content">
+        <h1>Privacy policy</h1>
+        Alle gegevens die je invoert op Skeer staan alleen lokaal opgeslagen binnen je brower (of app).
+        Door simpelweg je browser geschiedenis te verwijderen of de app te deinstalleren zijn al deze gegevens weg.
+        <br /><br />
+        We werken aan de mogelijkheid om deze gegevens aan een persoonlijk account te koppelen, zodat je push notificaties
+        kan instellen voor bepaalde producten.
+        <br /><br />
+        Skeer is een app door Stofkat/Noka Development, wij hechten veel waarde aan de privacy van onze gebruikers bij al onze apps.
+        Mocht je vragen hebben of je zorgen maken over je gegevens stuur dan gerust een email naar dirk@noka.dev.
+        <br /><br />
+        Dit is volledig optioneel. We vinden het zelf ook niet prettig als onze gegevens ongevraagd worden opgeslagen en doen dus ook zelf niet.
+        <br /><br />
+        Ok doei.
+        <br /><br />
+        <br /><br />
+        <b>English version</b>
+        <br /><br />
+
+        All data you enter on Skeer is only stored locally within your browser (or app).
+        Simply by deleting your browser history or uninstalling the app, all this data is gone.
+        <br /><br />
+        We are working on the possibility of linking this data to a personal account, so that you can set up push notifications for certain products.
+        <br /><br />
+        Skeer is an app by Stofkat/Noka Development, and we value the privacy of our users for all our apps.
+        If you have any questions or concerns about your data, please feel free to email dirk@noka.dev.
+        <br /><br />
+        This is completely optional. We also do not like it when our data is stored without our consent, so we do not do it ourselves.
+        <br /><br />
+        Okay, bye
+        <br /><br />
+        <br /><br />
+        <br /><br />
+        <br /><br />
+      </div>
     </div>
   );
 }
