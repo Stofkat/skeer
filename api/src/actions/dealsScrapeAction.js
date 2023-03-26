21ae565a93fd2c384200784781f0613640b60efc
Dirk de Boer <stofkat@gmail.com>
2023-03-26 13:42:48 +0200

    FUUUU

--- api/src/actions/dealsScrapeAction.js
+++ api/src/actions/dealsScrapeAction.js
@@ -1,8 +1,11 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-extra";
+import pluginStealth from "puppeteer-extra-plugin-stealth";
+import puppeteerExtraPluginUserAgentOverride from 'puppeteer-extra-plugin-stealth/evasions/user-agent-override/index.js';
 import moment from "moment";
 
-import stores from "../constants/stores.js";
+import stores, { testStores } from "../constants/stores.js";
 import Database from "../database/Database.js";
+import { executablePath } from "puppeteer";
 
 
 async function storeScrapedData(weekNumber, storeName, products) {
@@ -44,24 +47,94 @@ async function storeScrapedData(weekNumber, storeName, products) {
 }
 
 async function productScraper({ url, selectors }) {
+
+  let pageFullyScrolled = false;
+  const productsParsed = {};
+
+  puppeteer.use(pluginStealth());
+
+
+
+  function preload(device) {
+    Object.defineProperty(navigator, 'platform', {
+      value: device.platform,
+      writable: true,
+    });
+    Object.defineProperty(navigator, 'userAgent', {
+      value: device.userAgent,
+      writable: true,
+    });
+    Object.defineProperty(screen, 'height', {
+      value: device.viewport.height,
+      writable: true,
+    });
+    Object.defineProperty(screen, 'width', {
+      value: device.viewport.width,
+      writable: true,
+    });
+    Object.defineProperty(window, 'devicePixelRatio', {
+      value: device.viewport.deviceScaleFactor,
+      writable: true,
+    });
+  }
+
+
+  const device = {
+    userAgent: 'Mozilla/5.0 (Macintosh)',
+    viewport: {
+      width: 1200,
+      height: 800,
+      deviceScaleFactor: 1,
+      isMobile: false,
+      hasTouch: false,
+      isLandscape: true,
+    },
+    locale: 'en-US,en;q=0.9',
+    platform: 'Macintosh',
+  };
+
+  const pluginUserAgentOverride = puppeteerExtraPluginUserAgentOverride({
+    userAgent: device.userAgent,
+    locale: device.locale,
+    platform: device.platform,
+  });
+
+
+
+  puppeteer.use(pluginUserAgentOverride);
+
+
   const browser = await puppeteer.launch({
-    headless: false, // Set headless to false to open the browser window
-    args: ['--no-sandbox']
+    headless: false,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--shm-size=1gb'
+    ],
+    executablePath: executablePath()
   });
 
   const page = await browser.newPage();
-  //await page.setRequestInterception(true);
+  await page.evaluateOnNewDocument(preload, device);
 
-  // page.on("request", (req) => {
-  //     if(req.resourceType() == "font" || req.resourceType() == "image"){
-  //         req.abort();
-  //     }
-  //     else {
-  //         req.continue();
-  //     }
-  // });
+  await page.setRequestInterception(true);
+
+  page.on("request", (req) => {
+    if (req.resourceType() == "font" || req.resourceType() == "image") {
+      req.abort();
+    }
+    else {
+      req.continue();
+    }
+  });
+
 
   await page.goto(url);
+
+
+  // await page.screenshot({
+  //   path: 'screenshot.jpg'
+  // });
 
   if (selectors.cookie) {
     try {
@@ -71,117 +144,166 @@ async function productScraper({ url, selectors }) {
   }
 
 
-  await page.evaluate(async () => {
-    await new Promise((resolve) => {
 
-      let totalHeight = 0;
-      const distance = 100;
-      const scrollDelay = 200;
+  const parseThePage = async () => {
 
-      const timer = setInterval(() => {
-        let scrollHeight = document.body.scrollHeight;
-        window.scrollBy(0, distance);
-        totalHeight += distance;
+    let products = [];
+    try {
+      products = await page.$$(selectors.product);
+    } catch (error) {
+      console.error("Could not find products", error);
+    }
 
-        if (totalHeight >= scrollHeight) {
-          clearInterval(timer);
-          resolve();
-        }
+    for (let product of products) {
+      const data = await product.evaluate((el, selectors) => {
 
-      }, scrollDelay);
+        let description = null;
+        let price = "";
+        let url = "";
+        let img = "";
+        let name = "";
 
-    });
-  });
+        try {
+          name = el.querySelector(selectors.name).innerText.trim();
+        } catch { }
 
-
-  await page.waitForSelector(selectors.product, { timeout: 10000 });
-
-  const products = await page.$$(selectors.product);
-  const productsParsed = [];
-
-  for (let product of products) {
-    const data = await product.evaluate((el, selectors) => {
-
-      let description = null;
-      let price = "";
-      let url = "";
-      let img = "";
-      let name = "";
-
-
-      console.log("element", el);
-
-
-      try {
-        console.log("getting name");
-        name = el.querySelector(selectors.name).innerText.trim();
-        console.log("name", name);
-      } catch (error) {
-        console.error(error);
-      }
-
-      try {
-        if (el.href) {
-          url = el.href.trim();
-        } else {
-          url = el.querySelector(selectors.url).href.trim();
-        }
-      } catch { }
-      try {
-
-        img = el.querySelector(selectors.image).src.trim();
-
-        if (img.startsWith("data:image")) {
-          img = el.querySelector(selectors.image).getAttribute("srcset")?.trim();
-          if (!img) {
-            img = el.querySelector(selectors.image).getAttribute("data-src")?.trim();
+        try {
+          if (el.href) {
+            url = el.href.trim();
+          } else {
+            url = el.querySelector(selectors.url).href.trim();
           }
-        }
-      } catch { }
+        } catch { }
+
+        try {
+          img = el.querySelector(selectors.image).src.trim();
+
+          if (img.startsWith("data:image") || img.endsWith("svg")) {
+            img = el.querySelector(selectors.image).getAttribute("srcset")?.trim();
+
+            if (!img) {
+              img = el.querySelector(selectors.image).getAttribute("data-src")?.trim();
+            }
+
+            if (!img) {
+              img = el.querySelector(selectors.image).getAttribute("data-srcset")?.trim();
+            }
+          }
+        } catch { }
+
+        try {
+          if (selectors.description) {
+            description = el.querySelector(selectors.description).innerText.trim();
+          }
+        } catch { }
+
+        try {
+          if (selectors.price) {
+            price = el.querySelector(selectors.price).innerText.trim();
+          } else if (selectors.priceAlt) {
+            const priceDec = el.querySelector(selectors.priceAlt.dec).innerText.trim();
+            const priceFract = el.querySelector(selectors.priceAlt.fract).innerText.trim();
+            price = `${priceDec}${priceFract}`;
+          }
+        } catch { price = "n.a." }
+
+        return { name, price, img, description, url };
+
+      }, selectors);
+
+      productsParsed[data.name + data.description] = data;
 
 
-      try {
-        if (selectors.description) {
-          description = el.querySelector(selectors.description).innerText.trim();
-        }
-      } catch { }
-
-      try {
-        if (selectors.price) {
-          price = el.querySelector(selectors.price).innerText.trim();
-        } else if (selectors.priceAlt) {
-          const priceDec = el.querySelector(selectors.priceAlt.dec).innerText.trim();
-          const priceFract = el.querySelector(selectors.priceAlt.fract).innerText.trim();
-          price = `${priceDec}${priceFract}`;
-        }
-      } catch { price = "n.a." }
-
-      return { name, price, img, description, url };
-
-    }, selectors);
-
-    productsParsed.push(data);
+    }
   }
 
-  console.log("productsParsed", productsParsed);
+  const moveThePage = async (scrolledHeight) => {
+    return await page.evaluate(async (scrolledHeight) => {
+      const distance = 500;
 
-  // await browser.close();
+  
+      return await new Promise((resolve, reject) => {
 
-  return productsParsed;
+        try {
+
+          let newPageHeight = document.body.offsetHeight;
+          window.scrollBy(0, distance);
+    
+          let newScrolledHeight = scrolledHeight;
+
+          newScrolledHeight += distance;
+
+          resolve({ newScrolledHeight, newPageHeight });
+
+        } catch (error) {
+          console.error(document.body);
+          reject(document.body);
+        }
+      });
+
+    }, scrolledHeight);
+  }
+
+
+
+  let scrolledHeight = 0;
+  let pageHeight = 100;
+
+
+  while (pageHeight >= scrolledHeight) {
+    console.log("Scraping page");
+    await parseThePage();
+    console.log("Scrolling page", { scrolledHeight, pageHeight });
+    const result = await moveThePage(scrolledHeight);
+
+    console.log("result", result);
+    const { newScrolledHeight, newPageHeight } = result;
+
+    scrolledHeight = newScrolledHeight;
+    pageHeight = newPageHeight;
+
+    console.log("scrolled height", scrolledHeight);
+    console.log("page height", pageHeight);
+    console.log("is scroll height bigger than page height", scrolledHeight > pageHeight);
+  }
+
+
+
+  console.log("Done with store.");
+
+  await browser.close();
+
+  const productKeys = Object.keys(productsParsed)
+
+  return productKeys.map((key) => {
+    return productsParsed[key];
+  });
+
 
 };
 
 
 async function dealsScrapeAction() {
   const weekNumber = "" + moment().week();
+  let storesToScrape = null
+
+  if (process.env.NODE_ENV === "production") {
+    storesToScrape = stores;
+  } else {
+    storesToScrape = testStores;
+  }
 
   for (const index in stores) {
-    const store = stores[index];
-    const products = await productScraper(store);
+    try {
+      const store = stores[index];
+      const products = await productScraper(store);
 
-    // Only write on a succesful scrape
-    if (products.length > 0 && products[0].name?.length > 0) {
-      storeScrapedData(weekNumber, store.name, products);
+      // Only write on a succesful scrape
+      if (products.length > 0 && products[0].name?.length > 0) {
+        storeScrapedData(weekNumber, store.name, products);
+      }
+    } catch (error) {
+      console.error(error);
     }
 
   }
--- api/src/constants/stores.js
+++ api/src/constants/stores.js
@@ -1,100 +1,100 @@
 const stores = [
-  // {
-  //   name: "lidl",
-  //   label: "Lidl",
-  //   url: "https://www.lidl.nl/c/aanbiedingen/a10008785?tabCode=STORE_OFFER",
-  //   selectors: {
-  //     product: ".product-grid-box",
-  //     name: ".grid-box__headline",
-  //     image: ".product-grid-box__image",
-  //     cookie: ".cookie-alert-extended-button",
-  //     url: "a",
-  //     price: ".m-price__price"
-  //   }
-  // },
-  // {
-  //   name: "ah",
-  //   label: "Albert Heijn",
-  //   url: "https://www.ah.nl/bonus",
-  //   selectors: {
-  //     product: "[class^=promotion-card_root__]",
-  //     name: "[class^=typography_root__] span",
-  //     description: "[data-testhook=card-description] span",
-  //     image: "picture [class^=promotion-card-image_img__]",
-  //     cookie: "#accept-cookies",
-  //     url: "a",
-  //     priceAlt: {
-  //       dec: "[class^=promotion-price_root__] [class^=promotion-price_integer__]",
-  //       fract: "[class^=promotion-price_root__] [class^=promotion-price_fractional__]",
-  //     }
-  //   }
-  // },
-  // {
-  //   name: "dekamarkt",
-  //   label: "Dekamarkt",
-  //   url: "https://www.dekamarkt.nl/aanbiedingen",
-  //   selectors: {
-  //     product: ".deka-product-card",
-  //     name: ".deka-product-card--info--title",
-  //     image: "img",
-  //     cookie: ".cookie button",
-  //     url: "a",
-  //     priceAlt: {
-  //       dec: ".price--before-decimal--offer",
-  //       fract: ".price--after-decimal--offer",
-  //     }
-  //   }
-  // },
-  // {
-  //   name: "dirk",
-  //   label: "Dirk",
-  //   url: "https://www.dirk.nl/aanbiedingen",
-  //   selectors: {
-  //     product: ".product-card",
-  //     name: ".product-card__name",
-  //     image: ".product-card__image img",
-  //     description: ".product-card__description",
-  //     url: "a",
-  //     priceAlt: {
-  //       dec: ".product-card__price__euros",
-  //       fract: ".product-card__price__cents"
-  //     }
-  //   }
-  // },
-  // {
-  //   name: "aldi",
-  //   label: "Aldi",
-  //   url: "https://www.aldi.nl/aanbiedingen.html",
-  //   selectors: {
-  //     product: ".mod-article-tile--default",
-  //     name: ".mod-article-tile__title",
-  //     image: ".cq-dd-image",
-  //     description: ".mod-article-tile__brand",
-  //     url: "a",
-  //     price: ".price__wrapper",
-  //     // cookie: "[data-testid=uc-accept-all-button]"
+  {
+    name: "ah",
+    label: "Albert Heijn",
+    url: "https://www.ah.nl/bonus",
+    selectors: {
+      product: "[class^=promotion-card_root__]",
+      name: "[class^=typography_root__] span",
+      description: "[data-testhook=card-description] span",
+      image: "picture [class^=promotion-card-image_img__]",
+      cookie: "#accept-cookies",
+      url: "a",
+      priceAlt: {
+        dec: "[class^=promotion-price_root__] [class^=promotion-price_integer__]",
+        fract: "[class^=promotion-price_root__] [class^=promotion-price_fractional__]",
+      }
+    }
+  },
+  {
+    name: "lidl",
+    label: "Lidl",
+    url: "https://www.lidl.nl/c/aanbiedingen/a10008785?tabCode=STORE_OFFER",
+    selectors: {
+      product: ".product-grid-box",
+      name: ".grid-box__headline",
+      image: ".product-grid-box__image",
+      cookie: ".cookie-alert-extended-button",
+      url: "a",
+      price: ".m-price__price"
+    }
+  },
+  {
+    name: "dekamarkt",
+    label: "Dekamarkt",
+    url: "https://www.dekamarkt.nl/aanbiedingen",
+    selectors: {
+      product: ".deka-product-card",
+      name: ".deka-product-card--info--title",
+      image: "img",
+      cookie: ".cookie button",
+      url: "a",
+      priceAlt: {
+        dec: ".price--before-decimal--offer",
+        fract: ".price--after-decimal--offer",
+      }
+    }
+  },
+  {
+    name: "dirk",
+    label: "Dirk",
+    url: "https://www.dirk.nl/aanbiedingen",
+    selectors: {
+      product: ".product-card",
+      name: ".product-card__name",
+      image: ".product-card__image img",
+      description: ".product-card__description",
+      url: "a",
+      priceAlt: {
+        dec: ".product-card__price__euros",
+        fract: ".product-card__price__cents"
+      }
+    }
+  },
+  {
+    name: "aldi",
+    label: "Aldi",
+    url: "https://www.aldi.nl/aanbiedingen.html",
+    selectors: {
+      product: ".mod-article-tile--default",
+      name: ".mod-article-tile__title",
+      image: ".cq-dd-image",
+      description: ".mod-article-tile__brand",
+      url: "a",
+      price: ".price__wrapper",
+      // cookie: "[data-testid=uc-accept-all-button]"
 
-  //   }
-  // },
+    }
+  },
 
-  // {
-  //   name: "spar",
-  //   label: "Spar",
-  //   url: "https://www.spar.nl/aanbiedingen/",
-  //   selectors: {
-  //     product: ".c-product-tile",
-  //     name: ".c-product-tile__meta a",
-  //     image: "img",
-  //     description: ".c-product-tile__meta",
-  //     url: "c-product-tile__meta a",
-  //     priceAlt: {
-  //       dec: ".c-price .c-price__base",
-  //       fract: ".c-price .c-price__mod"
-  //     }
-  //     // cookie: "[data-testid=uc-accept-all-button]"
+  {
+    name: "spar",
+    label: "Spar",
+    url: "https://www.spar.nl/aanbiedingen/",
+    selectors: {
+      product: ".c-product-tile",
+      name: ".c-product-tile__meta a",
+      image: "img",
+      description: ".c-product-tile__meta",
+      url: ".c-product-tile__image a",
+      priceAlt: {
+        dec: ".c-price .c-price__base",
+        fract: ".c-price .c-price__mod"
+      }
+      // cookie: "[data-testid=uc-accept-all-button]"
 
-  //   }
-  // },
+    }
+  },
 
   {
     name: "coop",
@@ -108,11 +108,46 @@ const stores = [
       url: "c-product-tile__meta a",
       price: "[data-testing-id=current-price]"
     }
+  },
+
+  {
+    name: "plus",
+    label: "Plus",
+    url: "https://www.plus.nl/aanbiedingen",
+    selectors: {
+      product: ".product-list-block",
+      name: ".product-tile__description",
+      image: "img",
+      description: ".product-tile__quantity",
+      url: "a",
+      price: "text-price"
+    }
     // cookie: "[data-testid=uc-accept-all-button]"
 
   },
 
 
 ];
+
+export const testStores = [
+  {
+    name: "ah",
+    label: "Albert Heijn",
+    url: "https://www.ah.nl/bonus",
+    selectors: {
+      product: "[class^=promotion-card_root__]",
+      name: "[class^=typography_root__] span",
+      description: "[data-testhook=card-description] span",
+      image: "picture [class^=promotion-card-image_img__]",
+      cookie: "#accept-cookies",
+      url: "a",
+      priceAlt: {
+        dec: "[class^=promotion-price_root__] [class^=promotion-price_integer__]",
+        fract: "[class^=promotion-price_root__] [class^=promotion-price_fractional__]",
+      }
+    }
+  },
+];
+
 
 export default stores;
